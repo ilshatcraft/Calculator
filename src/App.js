f6f5c934d0df6e36a94c95be967d8eeb1168dd70
ilshatcraft <ilshatcraft@gmail.com>
2022-05-14 18:13:38 +0300

    сделал функционал ввода значений

--- src/App.js
+++ src/App.js
@@ -10,66 +10,86 @@ import Form from 'react-bootstrap/Form'
 
  function App() {
 
+  let arr_ru = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я'];
+  let arr_RU = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ', 'Э', 'Ю', 'Я'];
+  let arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  let arr_EN = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
   const [equation,SetEquation]=useState('')
  
-  const inputs=(x)=>{
+  const inputs=(x,value)=>{
     switch (x) {
-      case 1:
-        SetEquation(equation,x);
+       case 1:
+        SetEquation(equation+value);
         break;
-      
-      default:
+      case 2:
         
-    }
+        var  l=(Object.values(equation))
+         l.splice(-1,1)
+         SetEquation(l.join(''));
+        break;
+
+      case 3:
+        SetEquation('');
+        break;
+
+      case 4:
+          if (Object.values(equation).includes(value) )
+          break;  
+       
+     }
+     console.log(equation)
   }
 
   return <div className='Body'>
 <div>
-    <Form.Label></Form.Label>
-    <Form.Control type="" placeholder="" />
-    
-
+  <div>
+    <input 
+    type="text" 
+    value={equation.replace(/[^0-9\.\,\%\/\*\-\+]/g, '')}
+    onChange={(e)=>{SetEquation(e.target.value);}}
+    ></input>
+</div>
 <ButtonGroup className='Buttons_holder' >
     <div>
-<div vertical className='Buttons_holder_row'>
-  <Button>AC</Button>
-  <Button>&#129188;</Button>
-  <Button>&#65285;</Button>
-  <Button>&#247;</Button>
+<div  className='Buttons_holder_row'>
+  <Button onClick={()=>inputs(3)}>AC</Button>
+  <Button onClick={()=>inputs(2)}>&#129188;</Button>
+  <Button onClick={()=>inputs(1,'%')}>&#65285;</Button>
+  <Button onClick={()=>inputs(1,'/')}>&#247;</Button>
 
   
 </div>
 
-<div vertical className='Buttons_holder_row'>
-  <Button >1</Button>
-  <Button>2</Button>
-  <Button>3</Button>
-  <Button>&#215;</Button>
+<div  className='Buttons_holder_row'>
+  <Button onClick={()=>inputs(1,1)}>1</Button>
+  <Button onClick={()=>inputs(1,2)}>2</Button>
+  <Button onClick={()=>inputs(1,3)}>3</Button>
+  <Button  onClick={()=>inputs(1,'*')}>&#215;</Button>
   
   
 </div>
 
-<div vertical className='Buttons_holder_row'>
-  <Button>4</Button>
-  <Button>5</Button>
-  <Button>6</Button>
-  <Button>-</Button>
+<div   className='Buttons_holder_row'>
+  <Button onClick={()=>inputs(1,4)}>4</Button>
+  <Button onClick={()=>inputs(1,5)}>5</Button>
+  <Button onClick={()=>inputs(1,6)}>6</Button>
+  <Button onClick={()=>inputs(1,'-')}>-</Button>
   
  
 </div>
 
-<div vertical className='Buttons_holder_row'>
-  <Button>7</Button>
-  <Button>8</Button>
-  <Button>9</Button>
-  <Button>+</Button>
+<div  className='Buttons_holder_row'>
+  <Button onClick={()=>inputs(1,7)}>7</Button>
+  <Button onClick={()=>inputs(1,8)}>8</Button>
+  <Button onClick={()=>inputs(1,9)}>9</Button>
+  <Button onClick={()=>inputs(1,'+')}>+</Button>
   </div>
 
-  <div vertical className='Buttons_holder_row_last'>
-  <Button>,</Button>
-  <Button>0</Button>
-  <Button>=</Button>
+  <div  className='Buttons_holder_row_last'>
+  <Button onClick={()=>inputs(1,',')}>,</Button>
+  <Button onClick={()=>inputs(1,0)}>0</Button>
+  <Button onClick={()=>inputs(4)}>=</Button>
   </div>
 
 </div>
